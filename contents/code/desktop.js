56998bc5aabf02ecb3f3c68e36296e7d84928336
Baris Demirdelen <baris.demirdelen@sim-ci.com>
2018-03-16 14:30:53 +0100

    Add documentation on desktop

--- contents/code/desktop.js
+++ contents/code/desktop.js
@@ -1,11 +1,17 @@
-/** @constructor */
+/**
+ * Desktop class, Desktops may contain multiple screens,
+ * @constructor
+ * @param {int} id - KWin id of the desktop.
+ * */
 function Desktop(id) {
 
     var self = this;
 
+    /** @member {int} - Desktop id.*/
     this.id = id;
-
+    /** @member {list} - List of screens in the desktop. */
     this.screens = [];
+
     for (var i = 0; i < workspace.numScreens; i++) {
         this.screens[i] = new Screen(i);
     }
